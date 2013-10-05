bfa2df38c99e9a5647e9e0e85f65c7060d8697a0
Johan Andersson <nilsjohanandersson@gmail.com>
2013-10-05 12:07:22 +0000

    read users after reading issues

--- issue.lisp
+++ issue.lisp
@@ -1,9 +1,28 @@
 (in-package #:supportcentre)
 
 (defclass issue (storable)
-  ((subject :initarg :subject :accessor issue-subject)))
+  ((subject :initarg :subject :accessor issue-subject)
+   (creator :initarg :creator :accessor issue-creator)))
 
 (defmethod serialize ((issue issue))
   (with-output-to-string (out)
-    (prin1 (list :subject (issue-subject issue))
+    (prin1 (list :subject (issue-subject issue)
+                 :creator (storage-id (issue-creator issue)))
            out)))
+
+(defun set-users (issues)
+  (let* ((user-ids (mapcar #'issue-creator issues))
+         (users (storage-read-many 'user user-ids)))
+    (mapcar (lambda (issue user)
+              (setf (issue-creator issue) user)
+              issue)
+          issues
+          users)))
+
+(defmethod storage-read-many :around ((type (eql 'issue)) ids)
+  (set-users (call-next-method)))
+
+(defmethod storage-read :around ((type (eql 'issue)) ids)
+  (let ((user (call-next-method)))
+    (set-users (list user))
+    user))
