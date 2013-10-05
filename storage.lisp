2062f8cc281662ba3aadd653399acef212946201
Johan Andersson <nilsjohanandersson@gmail.com>
2013-10-05 09:48:59 +0000

    Store storables in sets

--- storage.lisp
+++ storage.lisp
@@ -1,7 +1,8 @@
 (in-package #:supportcentre)
 
 (defclass storable ()
-  ((id :initarg :id :accessor storage-id)))
+  ((id :initarg :id :accessor storage-id)
+   (sets :initarg :sets :initform nil :accessor storage-sets)))
 
 (defgeneric serialize (thing)
   (:method ((thing list))
@@ -12,7 +13,8 @@
   (:method (type (string (eql nil)))
     nil)
   (:method ((type (eql 'list)) string)
-    (values (safe-read string)))
+    (when string
+      (values (safe-read string))))
   (:method ((type symbol) (string string))
     (let ((properties (safe-read string)))
       (apply #'make-instance type properties))))
@@ -28,8 +30,12 @@
 (defgeneric storage-create (thing)
   (:method ((thing storable))
     (let ((serialized (serialize thing))
-          (key (storage-key thing)))
-      (red:setnx key serialized))))
+          (key (storage-key thing))
+          (sets-key (sets-key (storage-type thing) (storage-id thing))))
+      (prog1 (red:set key serialized)
+        (red:set sets-key (serialize (storage-sets thing)))
+        (dolist (set (storage-sets thing))
+          (red:sadd (set-key (storage-type thing) set) (storage-id thing)))))))
 
 (defmethod storage-create :before (storable)
   (let ((id (red:incr (next-id-key storable))))
@@ -37,17 +43,45 @@
 
 (defgeneric storage-read (type id)
   (:method ((type symbol) id)
-    (let* ((key (make-key type id))
-           (serialized (red:get key)))
-      (deserialize type serialized))))
+    (apply #'create type id (redis:with-pipelining
+                              (red:get (make-key type id))
+                              (red:get (sets-key type id))))))
+
+(defun create (type id thing-string sets-string)
+  (let ((thing (deserialize type thing-string))
+        (sets (deserialize 'list sets-string)))
+    (setf (storage-id thing) id
+          (storage-sets thing) sets)
+    thing))
+
+(defgeneric storage-read-set (type set)
+  (:method ((type symbol) set)
+    (let ((ids (red:smembers (set-key type set))))
+      (mapcar #'(lambda (id thing-string set-string)
+                  (create type id thing-string set-string))
+              ids
+              (redis:with-pipelining
+                (dolist (id ids)
+                  (red:get (make-key type id))))
+              (redis:with-pipelining
+                (dolist (id ids)
+                  (red:get (sets-key type id))))))))
 
 (defgeneric storage-update (storable))
 
 (defun next-id-key (storable)
-  (make-key "next-id" (storage-type storable)))
+  (make-key 'next-id (storage-type storable)))
+
+(defun set-key (type set)
+  (make-key type 'set set))
+
+(defun sets-key (type id)
+  (make-key type id 'sets))
 
 (defun next-id (storable)
   (red:incr (next-id-key storable)))
 
-(defun make-key (prefix suffix)
-  (format nil "~A:~A" prefix suffix))
+(defun make-key (prefix suffix &rest more)
+  (if more
+      (make-key prefix (apply #'make-key suffix more))
+      (format nil "~A:~A" prefix suffix)))
