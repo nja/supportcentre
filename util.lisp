3bf798815a8a03755b05a1f44f69cc21fa0ee467
Johan Andersson <nilsjohanandersson@gmail.com>
2013-10-09 19:04:25 +0000

    removed unused util stuff

--- util.lisp
+++ util.lisp
@@ -1,11 +1,5 @@
 (in-package #:supportcentre)
 
-(defun make-key (prefix suffix)
-  (format nil "~a:~a" (symbol-name prefix) suffix))
-
 (defun safe-read (string)
   (let ((*read-eval* nil))
     (read-from-string string)))
-
-(defun set-id (plist id)
-  (nconc (list :id id) plist))
