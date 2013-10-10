d78a4dec3ae539f786ae1f88d1d7a7bef5d33929
Johan Andersson <nilsjohanandersson@gmail.com>
2013-10-10 00:27:55 +0000

    user realname slot

--- user.lisp
+++ user.lisp
@@ -1,9 +1,11 @@
 (in-package #:supportcentre)
 
 (defclass user (storable)
-  ((name :initarg :name :accessor user-name)))
+  ((name :initarg :name :accessor user-name)
+   (realname :initarg :realname :accessor user-realname)))
 
 (defmethod serialize ((user user))
   (with-output-to-string (out)
-    (prin1 (list :name (user-name user))
+    (prin1 (list :name (user-name user)
+                 :realname (user-realname user))
            out)))
