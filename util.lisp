55e0130373d2b5d543967d379700ba0e0e5aa871
Johan Andersson <nilsjohanandersson@gmail.com>
2013-10-10 00:27:55 +0000

    compile-templates util function

--- util.lisp
+++ util.lisp
@@ -8,3 +8,9 @@
   (hunchentoot:escape-for-html
    (with-output-to-string (out)
      (prin1 object out))))
+
+(defun compile-templates ()
+  (closure-template:compile-cl-templates
+   (directory
+    (merge-pathnames "templates/*.tmpl"
+                     (asdf:component-pathname (asdf:find-system '#:supportcentre))))))
