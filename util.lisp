191948680efea9eb6f28cd154df050dc64fc8666
Johan Andersson <nilsjohanandersson@gmail.com>
2013-10-10 00:27:55 +0000

    remove util test stuff

--- util.lisp
+++ util.lisp
@@ -4,12 +4,6 @@
   (let ((*read-eval* nil))
     (read-from-string string)))
 
-(defun your-session (&optional stream)
-  (let ((session hunchentoot:*session*))
-    (if session
-        (format stream "Your session is ~D" (hunchentoot:session-id session))
-        (format stream "You have no session"))))
-
 (defun as-text (object)
   (hunchentoot:escape-for-html
    (with-output-to-string (out)
