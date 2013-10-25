2c70c2ff60737182e6c547eca0f98ba75f42405e
Johan Andersson <nilsjohanandersson@gmail.com>
2013-10-25 06:02:17 +0000

    area is timed, display change time in area list

--- area.lisp
+++ area.lisp
@@ -1,6 +1,6 @@
 (in-package #:supportcentre)
 
-(defclass area (storable linkable)
+(defclass area (storable linkable timed)
   ((name :initarg :name :accessor name-of)
    (owner :initarg :owner :accessor user-of)))
 
